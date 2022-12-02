bbe15fa72950464a85fea9296aafe93e9e5d8d82
binhussen <mhdhsna@gmail.com>
2022-12-02 11:23:35 +0300

    Add validation to models

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,9 +1,14 @@
 class Post < ActiveRecord
-  after_save :update_posts_counter
+  validates :likes_counter, :comments_counter, comparison: { greater_than_or_equal_to: 0 },
+            numericality: { only_integer: true }
+  validates :title, presence: true, length: { in: 1..250 }
+
   belongs_to :author, class_name: 'User'
   has_many :comments, dependent: :destroy
   has_many :likes, dependent: :destroy
 
+
+  after_save :update_posts_counter
   def recent_five
     comments.order(created_at: :desc).limit(5)
   end
