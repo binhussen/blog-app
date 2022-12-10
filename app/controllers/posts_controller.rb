1ddea2ccd01ef91c0ab06545a5fb1c870f8f5604
bin hussen <mhdhsna@gmail.com>
2022-12-10 14:22:18 +0300

    Create Like Controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,5 @@
 class PostsController < ApplicationController
+  before_action :set_user, only: %i[index create show]
   def index
     set_user
     @posts = @user.posts
@@ -9,6 +10,22 @@ class PostsController < ApplicationController
     set_post
   end
 
+  def create
+    @post = Post.new(post_params)
+    @post.author = current_user
+    @post.likes_counter = 0
+    @post.comments_counter = 0
+    if @post.save
+      redirect_to user_posts_path(current_user)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def new
+    @post = Post.new
+  end
+
   private
 
   def set_post
@@ -17,5 +34,9 @@ class PostsController < ApplicationController
 
   def set_user
     @user = User.find(params[:user_id])
+  end
+
+  def post_params
+    params.require(:post).permit(:title, :text)
   end
 end
